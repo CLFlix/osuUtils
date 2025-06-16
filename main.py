6c3d6c66e33097a12ad2abe96c35e9c544a5a736
CLFlix <remcoslootmaekers@gmail.com>
2025-06-16 11:44:11 +0200

    Commented out if-else statement in main.py

--- main.py
+++ main.py
@@ -44,32 +44,30 @@ def main():
             case _:
                 print('Invalid choice.')
                 time.sleep(2)
-            
-
                 
 
-        if choice == 0:
-            log('Quitting script~~')
-            run = False
+        # if choice == 0:
+        #     log('Quitting script~~')
+        #     run = False
     
-        elif choice == 1:
-            src = input("File that needs to be copied [Full path]\n")
-            dest = input("Location & file base name [Full path]\n")
-            file_type = input("What type of file do you want to copy? (txt, png, jpg,...)\n")
-            amount = int(input("Amount of copies\n"))
+        # elif choice == 1:
+        #     src = input("File that needs to be copied [Full path]\n")
+        #     dest = input("Location & file base name [Full path]\n")
+        #     file_type = input("What type of file do you want to copy? (txt, png, jpg,...)\n")
+        #     amount = int(input("Amount of copies\n"))
 
-            copy_files(src, dest, amount, file_type)
-            time.sleep(2)
+        #     copy_files(src, dest, amount, file_type)
+        #     time.sleep(2)
 
-        elif choice == 2:
-            run_stream_tools()
-            time.sleep(2)
+        # elif choice == 2:
+        #     run_stream_tools()
+        #     time.sleep(2)
 
-        elif choice == 3:
-            close_stream_tools()
-            time.sleep(2)
+        # elif choice == 3:
+        #     close_stream_tools()
+        #     time.sleep(2)
 
-        else:
-            print("Invalid choice. Please try again.")
+        # else:
+        #     print("Invalid choice. Please try again.")
 
 main()
